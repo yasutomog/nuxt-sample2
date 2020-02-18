125be4d8fb8c91c74593149a2ec1660b93569e80
yasutomog <yasutomo.goto@gmail.com>
2020-02-19 00:25:02 +0900

    create form page4

---
+++ src/components/Form.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <form name="contact" method="POST" netlify>
+      <input type="hidden" name="form-name" value="contact">
+      <p>
+        <label>Your Name: <input type="text" name="name" placeholder="name"></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" placeholder="email"></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message" placeholder="msg"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
+</template>
--- src/pages/form/index.vue
+++ src/pages/form/index.vue
@@ -1,29 +1,17 @@
 <template>
   <div>
-    <logo />
-    <form name="contact" method="POST" netlify>
-      <input type="hidden" name="form-name" value="contact">
-      <p>
-        <label>Your Name: <input type="text" name="name" placeholder="name"></label>
-      </p>
-      <p>
-        <label>Your Email: <input type="email" name="email" placeholder="email"></label>
-      </p>
-      <p>
-        <label>Message: <textarea name="message" placeholder="msg"></textarea></label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <contact />
   </div>
 </template>
 <script>
 import Logo from '~/components/Logo.vue'
+import Contact from '~/components/Form.vue'
+
 
 export default {
   components: {
-    Logo
+    Logo,
+    Contact
   }
 }
 </script>
