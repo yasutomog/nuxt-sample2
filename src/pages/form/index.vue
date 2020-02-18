c05270cfb14fa0a7d642fc383d6718d134ed8ff1
yasutomog <yasutomo.goto@gmail.com>
2020-02-18 19:23:04 +0900

    create form page2

--- src/pages/form/index.vue
+++ src/pages/form/index.vue
@@ -1,24 +1,18 @@
 <template>
-  <div>
-    <client-only>
-      <form name="contact" method="POST" netlify>
-        <input type="hidden" name="form-name" value="contact">
-        <p>
-          <label>Your Name: <input type="text" name="name" placeholder="後藤"></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message" placeholder="テストメッセージ" /></label>
-        </p>
-        <p>
-          <button type="submit">
-            Send
-          </button>
-        </p>
-      </form>
-    </client-only>
-  </div>
+  <client-only>
+    <form name="contact" method="POST" netlify>
+      <input type="hidden" name="form-name" value="contact">
+      <p>
+        <label>Your Name: <input type="text" name="name" placeholder="後藤"></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message" placeholder="テストメッセージ" /></label>
+      </p>
+      <p>
+        <button type="submit">
+          Send
+        </button>
+      </p>
+    </form>
+  </client-only>
 </template>
-<script>
-export default {
-}
-</script>
